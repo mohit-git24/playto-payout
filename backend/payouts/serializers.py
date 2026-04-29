c6e2f41bc2f566126cc4394e116d87244b4e7a6f
mohit-git1 <babafrusty2826@gmail.com>
2026-04-29 14:01:16 +0530

    fix: include bank_accounts in merchant serializer

--- backend/payouts/serializers.py
+++ backend/payouts/serializers.py
@@ -2,16 +2,18 @@ from rest_framework import serializers
 from .models import Merchant, Payout, LedgerEntry, BankAccount
 
 
-class MerchantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Merchant
-        fields = ['id', 'name', 'email', 'created_at']
-
-
 class BankAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = BankAccount
         fields = ['id', 'account_number', 'ifsc_code', 'account_holder_name']
+
+
+class MerchantSerializer(serializers.ModelSerializer):
+    bank_accounts = BankAccountSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Merchant
+        fields = ['id', 'name', 'email', 'created_at', 'bank_accounts']
 
 
 class LedgerEntrySerializer(serializers.ModelSerializer):
